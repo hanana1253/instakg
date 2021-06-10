a778ae1c511a60471f65efd09d7741dff28be55c
hanana1253 <hanana1253@gmail.com>
2021-06-10 13:18:32 +0900

    feat: Create Basefields and profile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from behaviors import BaseFields
 
 # Create your models here.
+class Profile(BaseFields):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=8)
+    introduction = models.TextFields(null=True, blank=True)
--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'authentication',
+    'posting',
+    'social',
 ]
 
 MIDDLEWARE = [
