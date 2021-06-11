3b9519d076511d0c34001cf3f8c9f2e1af42268c
hanana1253 <hanana1253@gmail.com>
2021-06-11 17:00:09 +0900

    feat: Create PostingDetailView

--- posting/urls.py
+++ posting/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from posting.views import PostingAddView, PostingEditView
+from posting.views import PostingAddView, PostingEditView, PostingDetailView, CommentAddView
 
 app_name = 'posting'
 
 urlpatterns = [
     path('new/', PostingAddView.as_view(), name='add'),
     path('edit/<int:pk>', PostingEditView.as_view(), name='edit'),
+    path('<int:pk>', PostingDetailView.as_view(), name='detail'),
+    path('comment/<int:posting_pk>', CommentAddView.as_view(), name='comment'),
 ]
--- posting/views.py
+++ posting/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.views import View
+from django.views import View, generic
+from posting.models import Posting
 from posting.services import PostingService
 from posting.dto import PostingDto
 
@@ -8,6 +9,11 @@ class IndexTimelineView(View):
     def get(self, request, *args, **kwargs):
         context = { 'postings_list': PostingService.find_all() }
         return render(request, 'index.html', context)
+
+class PostingDetailView(generic.DetailView):
+    template_name = 'posting_detail.html'
+    model = Posting
+    context_object_name = 'posting'
 
 class PostingAddView(View):
     def get(self, request, *args, **kwargs):
@@ -29,6 +35,13 @@ class PostingAddView(View):
         )
 
 class PostingEditView(View):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+class CommentAddView(View):
     def get(self, request, *args, **kwargs):
         pass
 
